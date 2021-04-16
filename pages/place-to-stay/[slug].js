3d5151b372fb57599f8931ff22af1117e00e829a
Alexandra Spalato <alexaspalato@gmail.com>
2021-04-16 19:39:35 +0200

    layout

--- pages/place-to-stay/[slug].js
+++ pages/place-to-stay/[slug].js
@@ -11,6 +11,14 @@ const PlaceToStay = ({ ptsData = {} }) => {
   return (
     <>
       <Title title={title} intro="Recommended place to stay:" />
+      <div className="container justify-center block px-5 md:px-0 lg:flex">
+        <div className="w-full mr-16 bg-darkBlue lg:w-3/4 mb-7 lg:mb-0 h-[800px]">
+          main
+        </div>
+        <div className="w-full bg-darkBlue lg:w-1/3 h-[800px] lg:-mt-32">
+          sidebar
+        </div>
+      </div>
     </>
   );
 };
