eb6218ca5c2a8d9a6255447834a9ee70590e1d0c
Alexandra Spalato <alexaspalato@gmail.com>
2021-04-17 10:13:56 +0200

    image config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withWPEHeadless = require('@wpengine/headless/nextConfig');
+const slashes = require('remove-trailing-slash');
+const imgHost = slashes(process.env.WP_URL).split('/')[2];
 
 module.exports = withWPEHeadless({
   // future: {
   //   webpack5: true,
   // },
+  images: {
+    domains: [`${imgHost}`],
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // Unset client-side javascript that only works server-side
