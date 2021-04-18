afeadc694dee07ed0cfd60cdd55f8d693cb70439
Alexandra Spalato <alexaspalato@gmail.com>
2021-04-18 21:43:24 +0200

    framer motion

--- components/ui-components/CollapseSection.js
+++ components/ui-components/CollapseSection.js
@@ -1,49 +1,58 @@
-import React from 'react';
-import { Disclosure, Transition } from '@headlessui/react';
+import React, { useState } from 'react';
+// import { Disclosure, Transition } from '@headlessui/react';
 import { FaChevronRight } from 'react-icons/fa';
 import { AnimatePresence, motion } from 'framer-motion';
 import clsx from 'clsx';
 
 export const CollapseSection = ({ children, className, title, ...props }) => {
+  const [open, toggleOpen] = useState(false);
   return (
-    <Disclosure>
-      {({ open }) => (
-        <section
-          className={clsx(
-            'px-9 py-8 my-base2',
-            'bg-white border shadow-section border-grey2',
-            'transition-max-h duration-500   ease-out',
-            className,
-          )}
-          {...props}>
-          <>
-            <Disclosure.Button className="flex items-center cursor-pointer focus:outline-none">
-              <FaChevronRight
-                className={clsx(
-                  'transition duration-500',
-                  'mr-5',
-                  'text-lightBlue text-[49px]',
-                  { 'transform rotate-90': open },
-                )}
-              />
-
-              <h3 className="text-grey5 text-f-36">{title}</h3>
-            </Disclosure.Button>
-            <Transition
-              show={open}
-              enter="transition duration-700 ease-out"
-              enterFrom="opacity-0  transform -translate-y-4"
-              enterTo="opacity-100 transform translate-y-0"
-              leave="transition duration-300 ease-out"
-              leaveFrom="opacity-100 transform translate-y-0"
-              leaveTo="transform opacity-0 -translate-y-4">
-              <Disclosure.Panel className={clsx('pt-5')} static>
-                {children}
-              </Disclosure.Panel>
-            </Transition>
-          </>
-        </section>
+    <section
+      className={clsx(
+        'px-9 py-8 my-base2',
+        'bg-white border shadow-section border-grey2',
+        'transition-max-h duration-500   ease-out',
+        className,
       )}
-    </Disclosure>
+      {...props}>
+      <>
+        <div
+          role="button"
+          className="flex items-center cursor-pointer focus:outline-none"
+          onClick={() => toggleOpen((prevState) => !prevState)}>
+          <FaChevronRight
+            className={clsx(
+              'transition duration-500',
+              'mr-5',
+              'text-lightBlue text-[49px]',
+              { 'transform rotate-90': open },
+            )}
+          />
+
+          <h3 className="text-grey5 text-f-36">{title}</h3>
+        </div>
+        <AnimatePresence>
+          {/* <Transition
+            show={open}
+            enter="transition duration-700 ease-out"
+            enterFrom="opacity-0  transform -translate-y-4"
+            enterTo="opacity-100 transform translate-y-0"
+            leave="transition duration-500 ease-out"
+            leaveFrom="opacity-100 transform translate-y-0"
+            leaveTo="transform opacity-0 -translate-y-4"> */}
+          {open && (
+            <motion.div
+              initial={{ height: 0, opacity: 0, y: 20 }}
+              animate={{ height: 'auto', opacity: 1, y: 0 }}
+              exit={{ height: 0, opacity: 0, y: 20 }}
+              transition={{ duration: 0.5 }}
+              className={clsx('pt-5 overflow-hidden')}>
+              {children}
+            </motion.div>
+          )}
+          {/* </Transition> */}
+        </AnimatePresence>
+      </>
+    </section>
   );
 };
