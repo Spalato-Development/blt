fe737bf277ee98cfd04109ab98f1a5850fad33d4
Carlos Loureda <carloslouredaparrado@gmail.com>
2021-04-26 17:07:54 +0200

    fix: Renamed Date component into DateFormatter

--- components/Date.js
+++ components/Date.js
@@ -1,6 +1,6 @@
 import { format } from 'date-fns';
 
-export const Date = ({ date }) => {
+export const DateFormatter = ({ date }) => {
   const formatDate = !!date && date.split(' ').join('T');
   return !!date && format(new Date(formatDate), 'MMMM dd, yyyy');
 };
