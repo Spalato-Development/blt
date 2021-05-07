439bc4453e604f417c0451af7308e17c76ccf17c
Alexandra Spalato <alexaspalato@gmail.com>
2021-05-07 11:07:44 +0200

    searchTabs responsive

--- components/About.js
+++ components/About.js
@@ -6,14 +6,16 @@ export const About = ({ writer = {}, date, text, children, ...props }) => {
   console.log('about writer', writer);
   return (
     <Fragment {...props}>
-      <div className="flex justify-between text-f-22">
+      <div className="justify-between text-f-18 sm:flex md:text-f-22">
         <div>
           Expert travel writer:{' '}
           <Link href={writer?.uri || ''}>
             <a>{writer?.title}</a>
           </Link>
         </div>
-        <div>Last updated: <DateFormatter date={date} /> </div>
+        <div>
+          Last updated: <DateFormatter date={date} />{' '}
+        </div>
       </div>
       <div className="mt-5 mb-12 prose-lg">{children}</div>
       <div className="text-center text-gold text-f-24">{text}</div>
--- components/search/SearchTabs.js
+++ components/search/SearchTabs.js
@@ -14,7 +14,7 @@ export const SearchTabs = ({
   return (
     <div
       className={clsx(
-        'flex flex-wrap justify-start -mx-1 md:justify-between sm:flex-nowrap',
+        'flex flex-wrap justify-start -mx-1 md:justify-between md:flex-nowrap',
         className,
       )}
       {...props}>
@@ -22,7 +22,7 @@ export const SearchTabs = ({
         <Button
           key={tab.name}
           value={tab.name}
-          className={clsx('h:[39px] sm:h-[54px]', 'w-[100px] sm:w-[149px]')}
+          className={clsx('h-[39px] sm:h-[54px]', 'w-auto md:w-[149px]')}
           onClick={(e) => handleClick(e)}
           tab>
           {tab.name}
