7673eddaa6d52b8a05b6fadb680160c36500d677
Alexandra Spalato <alexaspalato@gmail.com>
2021-05-11 11:13:44 +0200

    experience base page

---
+++ pages/experience/[slug].js
@@ -0,0 +1,80 @@
+import { getApolloClient } from '@wpengine/headless';
+import { GET_EXPERIENCE } from 'lib/queries';
+import { appGetStaticProps } from 'lib/appGetStaticProps';
+import { getNextStaticPaths } from '@wpengine/headless/next';
+import {
+  Title,
+  Tabs,
+  Gallery,
+  CollapseSection,
+  TravelQuote,
+  ContentLayout,
+} from 'components/ui-components';
+import { About, Newsletter } from 'components';
+
+const Experience = ({ experienceData = {} }) => {
+  const { experience } = experienceData.data || {};
+  const {
+    title,
+    modified,
+    commonDataAttributes: { imageGallery, about, standfirst },
+    customDataAttributes: { writer },
+  } = experience;
+
+  const tabs = [
+    { name: 'our review' },
+    { name: 'logistics' },
+    { name: 'who to go with' },
+    { name: 'where to stay' },
+    { name: 'map' },
+  ];
+  return (
+    <>
+      <Title title={title} intro="Best things to do:" />
+      <ContentLayout
+        sidebar={
+          <>
+            <Newsletter />
+          </>
+        }>
+        <Tabs tabs={tabs} className="mb-4" />
+        <Gallery images={imageGallery} />
+
+        {/* Review */}
+        <CollapseSection title="Our review" id="our-review">
+          <About
+            writer={writer && writer[0]}
+            date={modified}
+            text="Know someone who would like this place to stay? Why not let them know…">
+            <div dangerouslySetInnerHTML={{ __html: about }} />
+          </About>
+        </CollapseSection>
+      </ContentLayout>
+    </>
+  );
+};
+
+export default Experience;
+
+export const getStaticProps = async (context) => {
+  const client = getApolloClient(context);
+  const globalData = await appGetStaticProps(context);
+  const experienceData = await client.query({
+    query: GET_EXPERIENCE,
+    variables: {
+      id: context.params.slug,
+    },
+    errorPolicy: 'all',
+  });
+
+  return {
+    props: {
+      experienceData,
+      globalData,
+    },
+  };
+};
+
+export function getStaticPaths() {
+  return getNextStaticPaths();
+}
